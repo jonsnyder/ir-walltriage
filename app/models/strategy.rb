aeba5526faeae5710851a8ee32a571d890f815bb
Jon Snyder <jon@contextoptional.com>
2012-05-07 19:10:31 -0600

    fix to saving scores for NaN

--- app/models/strategy.rb
+++ app/models/strategy.rb
@@ -41,14 +41,22 @@ class Strategy < ActiveRecord::Base
       kappa = CohenKappa.new( post_ids, system_matcher, user_matcher)
       kappa.calculate
       
-      stat_values.create( :stat => Stat.kappa, :access_token => user, :value => kappa.score)
-      stat_values.create( :stat => Stat.precision, :access_token => user, :value => kappa.precision)
-      stat_values.create( :stat => Stat.recall, :access_token => user, :value => kappa.recall)
+      stat_values.create( :stat => Stat.kappa, :access_token => user, :value => fix( kappa.score))
+      stat_values.create( :stat => Stat.precision, :access_token => user, :value => fix( kappa.precision))
+      stat_values.create( :stat => Stat.recall, :access_token => user, :value => fix( kappa.recall))
     end
 
     tf_idfs = TfIdfCalculator.new( mallet_run.dataset.posts).calculate
     cs = CosineSimularity.new( tf_idfs, system_matcher).calculate
     stat_values.create( :stat => Stat.cosine_simularity, :value => (cs[0] / cs[1])) 
+  end
+
+  def fix( value)
+    if value == NaN
+      nil
+    else
+      value
+    end
   end
   
 end
